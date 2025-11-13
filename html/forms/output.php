6cdcba05a97657cdb5f80d13c428c513458c8c9e
chandangoyal655 <chandangoyal655@gmail.com>
2025-11-14 00:55:44 +0530

    successfully submit half form topics in forms branch

---
+++ html/forms/output.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>output of form</title>
+  </head>
+  <body>
+		<?php
+   echo <h1>YOUR DATA IS SUCCESSFULLY SUBMITTED</h1>
+    echo <p>we have received your input</p>
+		?>
+  </body>
+</html>
